912addf7457697923d7a41e048c1955a070d188c
sudiptonayek <55348515+sudiptonayek@users.noreply.github.com>
2025-06-09 21:13:10 +0530

    Update ResetPassword.jsx

--- frontend/src/landing_page/resetPassword/ResetPassword.jsx
+++ frontend/src/landing_page/resetPassword/ResetPassword.jsx
@@ -14,7 +14,7 @@ export default function ResetPassword() {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/api/client/reset-password/${id}/${token}`)
+      .get(`https://zerodhaapp-ajjs.onrender.com/api/client/reset-password/${id}/${token}`)
       .then(() => {
         setValid(true);
         setLoading(false);
