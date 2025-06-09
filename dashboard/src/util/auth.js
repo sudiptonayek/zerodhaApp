0d33caf1b00daf8cca6a58af6abcfb56f2acff26
sudiptonayek <55348515+sudiptonayek@users.noreply.github.com>
2025-06-09 21:19:52 +0530

    Update auth.js

--- dashboard/src/util/auth.js
+++ dashboard/src/util/auth.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const isAuthenticated = async ()=>{
     try{
-        const res = await axios.get("http://localhost:5000/api/client/verify",{withCredentials:true});
+        const res = await axios.get("https://zerodhaapp-ajjs.onrender.com/api/client/verify",{withCredentials:true});
         return res.data.user
     }catch(err){
         console.log("auth - ",err);
